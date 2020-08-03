0234944594c3393252ffbf70118d30b5c55bb073
Juliana <juliana.tfg@gmail.com>
2020-08-03 15:43:54 -0300

    Gatsby tutorial #3
    I did them all 🤩

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default function About() {
+  return (
+    <Layout>
+      <h1>About me. 🧙🏻‍♀️</h1>
+      <p>
+        I’m good enough, I’m smart enough, and <i>goddamnit</i>, people like me!
+      </p>
+    </Layout>
+  )
+}
